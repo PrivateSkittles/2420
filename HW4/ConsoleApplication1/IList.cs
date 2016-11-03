7781f9542637bc7cf6eefa0a4bbdf4a0b880738b
Lipio <Lipio@DESKTOP-KM3VKMH>
2016-11-02 22:42:41 -0600

    80% progress

---
+++ HW4/ConsoleApplication1/IList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+
+
+class List<T>
+{
+    T[] underlyingArray = new T[3];
+
+    public void Add(T value)
+    {
+        if (Count == underlyingArray.Length)
+            growUnderlyingArray();
+        underlyingArray[Count] = value;
+        Count++;
+    }
+
+    private void growUnderlyingArray()
+    {
+        T[] newArray = new T[underlyingArray.Length * 2];
+        for (int i = 0; i < underlyingArray.Length; i++)
+            newArray[i] = underlyingArray[i];
+        underlyingArray = newArray;    
+    }
+
+    public T this[int index]
+    {
+        get
+        {
+            if (index >= Count)
+                throw new IndexOutOfRangeException();
+            return underlyingArray[index];
+        }
+
+        set
+        {
+            if (index >= Count)
+                throw new IndexOutOfRangeException();
+            underlyingArray[index] = value;
+        }
+    }
+
+    public int Count { get; private set; }
+}
--- HW4/ConsoleApplication1/Main.cs
+++ HW4/ConsoleApplication1/Main.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using System;
+using System.Diagnostics;
+
 
 class MainClass
 {
@@ -26,5 +28,16 @@ class MainClass
         Console.WriteLine("Quick Sorter");
         theGauntlet.beatUpASorter(new quickSorter());
         Console.WriteLine();
+
+        List<string> myList = new List<string>();
+        Debug.Assert(myList.Count == 0);
+        myList.Add("Jamie");
+        myList.Add("Bob");
+        myList.Add("Jill");
+        Debug.Assert(myList.Count == 3);
+
+        List<int> myListOfInts = new List<int>();
+        myListOfInts.Add(5);
+        myListOfInts.Add(7);
     } 
 }
