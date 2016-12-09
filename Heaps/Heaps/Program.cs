29630a499b8063f03be1430bb1be0e1fc62e9116
lind <lind@lab9-41>
2016-12-09 15:22:09 -0700

    Heaps 80% (70% skipped)

--- Heaps/Heaps/Program.cs
+++ Heaps/Heaps/Program.cs
@@ -1,15 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 
 class BinaryHeap
 {
-    List<int> heapList = new List<int>(new int[] { 0 });
+    public List<int> heapList = new List<int>(new int[] { 0 });
     int currentSize = 0;
 
     public void printList()
     {
-        foreach (int value in heapList)
+        for (int i = 1; i < heapList.Count ; i++)
         {
-            System.Console.WriteLine(value);
+            System.Console.WriteLine(heapList[i]);
         }
     }
 
@@ -34,12 +35,14 @@ class BinaryHeap
         }
     }
 
-    public void popTop()
+    public int popTop()
     {
+        int valueBeingPopped = heapList[1];
         heapList[1] = heapList[currentSize];
         heapList.RemoveAt(currentSize);
         currentSize -= 1;
         sink(1);
+        return valueBeingPopped;
     }
 
     private void sink(int sinkingValue)
@@ -59,15 +62,20 @@ class BinaryHeap
 
     private int findMinChild(int i)
     {
-        if ((i * 2 + 1) > currentSize)
+        if ((i * 2 + 1) > currentSize) //if there is only one child, return that child
             return i * 2;
-        else
+        else //returns child with smaller value
         {
             if (heapList[i * 2] < heapList[i * 2 + 1])
                 return i * 2;
             else
                 return i * 2 + 1;
         }
+    }
+
+    public void heapSort()
+    {
+
     }
  
 }
@@ -87,9 +95,21 @@ class MainClass
         binaryHeap.add(2);
         binaryHeap.add(4);
 
-        binaryHeap.popTop();
-        binaryHeap.popTop();
-
+        System.Console.WriteLine("Binary Heap with values 5, 2, 1, 21, 12, 2, 4 added");
         binaryHeap.printList();
+        System.Console.WriteLine();
+
+        System.Console.WriteLine("Binary Heap popTop assert test");
+        //loop pops until heap is empty and asserts each value popped is larger than previous popped value
+        for (int previousPop = binaryHeap.popTop(); binaryHeap.heapList.Count != 1;)
+        {
+            binaryHeap.printList();
+            int currentPop = binaryHeap.popTop();
+            Debug.Assert(previousPop <= currentPop);
+            previousPop = currentPop;
+            System.Console.WriteLine();
+        }
+
+        System.Console.WriteLine("Heap empty");
     }
 }
