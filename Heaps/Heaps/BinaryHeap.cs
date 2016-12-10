083451fbf7bfd1cfbcab39755d5cd72f4b2279d0
Josh Lipio <why.am.i.seriously@gmail.com>
2016-12-10 15:22:01 -0700

    Heaps 100%

---
+++ Heaps/Heaps/BinaryHeap.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+class BinaryHeap
+{
+    public List<int> heapList = new List<int>(new int[] { 0 });
+    int currentSize = 0;
+
+    public void printList()
+    {
+        for (int i = 1; i < heapList.Count; i++)
+        {
+            System.Console.WriteLine(heapList[i]);
+        }
+    }
+
+    public void add(int value)
+    {
+        heapList.Add(value);
+        currentSize += 1;
+        swim(currentSize);
+    }
+
+    private void swim(int addedValue)
+    {
+        while (addedValue / 2 > 0)
+        {
+            if (heapList[addedValue] > heapList[addedValue / 2])
+            {
+                int temp = heapList[addedValue / 2];
+                heapList[addedValue / 2] = heapList[addedValue];
+                heapList[addedValue] = temp;
+            }
+            addedValue = addedValue / 2;
+        }
+    }
+
+    public int popTop()
+    {
+        int valueBeingPopped = heapList[1];
+        heapList[1] = heapList[currentSize];
+        heapList.RemoveAt(currentSize);
+        currentSize -= 1;
+        sink(1);
+        return valueBeingPopped;
+    }
+
+    private void sink(int sinkingValue)
+    {
+        while ((sinkingValue * 2) <= currentSize)
+        {
+            int minChild = findMinChild(sinkingValue);
+            if (heapList[sinkingValue] < heapList[minChild])
+            {
+                int temp = heapList[sinkingValue];
+                heapList[sinkingValue] = heapList[minChild];
+                heapList[minChild] = temp;
+            }
+            sinkingValue = minChild;
+        }
+    }
+
+    private int findMinChild(int i)
+    {
+        if ((i * 2 + 1) > currentSize) //if there is only one child, return that child
+            return i * 2;
+        else //returns child with smaller value
+        {
+            if (heapList[i * 2] > heapList[i * 2 + 1])
+                return i * 2;
+            else
+                return i * 2 + 1;
+        }
+    }
+
+    public void buildHeap(List<int> aList)
+    {
+        int sinkingValue = (aList.Count) / 2;
+        currentSize = aList.Count - 1;
+        for (int i = 1; i < aList.Count; i++)
+        {
+            if (i > heapList.Count - 1) //edge case if heap being overwritten is smaller than heap being built
+                heapList.Add(aList[i]);
+            else //overwrites existing heap
+                heapList[i] = aList[i];
+        }
+        while (sinkingValue > 0) //builds heap
+        {
+            sink(sinkingValue);
+            sinkingValue -= 1;
+        }
+    }
+
+    public void heapSort()
+    {
+        for (int index = currentSize; index > 0; index--)
+        {
+            int temp = heapList[1];
+            heapList[1] = heapList[index];
+            heapList[index] = temp;
+            currentSize--;
+            sink(1);
+        }
+    }
+
+    public int this[int index]
+    {
+        get { return heapList[index + 1]; }
+    }
+
+    public void testInvariants()
+    {
+        for (int index = 1; index < currentSize / 2; index++)
+        {
+            Debug.Assert(heapList[index] > heapList[index * 2] && heapList[index] > heapList[index * 2 + 1]);
+        }
+    }
+}
--- Heaps/Heaps/Program.cs
+++ Heaps/Heaps/Program.cs
@@ -1,115 +1,55 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
 
-class BinaryHeap
-{
-    public List<int> heapList = new List<int>(new int[] { 0 });
-    int currentSize = 0;
-
-    public void printList()
-    {
-        for (int i = 1; i < heapList.Count ; i++)
-        {
-            System.Console.WriteLine(heapList[i]);
-        }
-    }
-
-    public void add(int value)
-    {
-        heapList.Add(value);
-        currentSize += 1;
-        swim(currentSize);
-    }
-
-    private void swim(int addedValue)
-    {
-        while (addedValue / 2 > 0)
-        {
-            if (heapList[addedValue] < heapList[addedValue / 2])
-            {
-                int temp = heapList[addedValue / 2];
-                heapList[addedValue / 2] = heapList[addedValue];
-                heapList[addedValue] = temp;
-            }
-            addedValue = addedValue / 2;
-        }
-    }
-
-    public int popTop()
-    {
-        int valueBeingPopped = heapList[1];
-        heapList[1] = heapList[currentSize];
-        heapList.RemoveAt(currentSize);
-        currentSize -= 1;
-        sink(1);
-        return valueBeingPopped;
-    }
-
-    private void sink(int sinkingValue)
-    {
-        while ((sinkingValue * 2) <= currentSize)
-        {
-            int minChild = findMinChild(sinkingValue);
-            if (heapList[sinkingValue] > heapList[minChild])
-            {
-                int temp = heapList[sinkingValue];
-                heapList[sinkingValue] = heapList[minChild];
-                heapList[minChild] = temp;           
-            }
-            sinkingValue = minChild;
-        }
-    }
-
-    private int findMinChild(int i)
-    {
-        if ((i * 2 + 1) > currentSize) //if there is only one child, return that child
-            return i * 2;
-        else //returns child with smaller value
-        {
-            if (heapList[i * 2] < heapList[i * 2 + 1])
-                return i * 2;
-            else
-                return i * 2 + 1;
-        }
-    }
-
-    public void heapSort()
-    {
-
-    }
- 
-}
-
-
 class MainClass
 {
     static void Main()
     {
         BinaryHeap binaryHeap = new BinaryHeap();
 
-        binaryHeap.add(5);
         binaryHeap.add(2);
-        binaryHeap.add(1);
-        binaryHeap.add(21);
+        binaryHeap.add(16);
         binaryHeap.add(12);
-        binaryHeap.add(2);
-        binaryHeap.add(4);
+        binaryHeap.add(7);
+        binaryHeap.add(5);
+        binaryHeap.add(12);
+        binaryHeap.add(1);
 
-        System.Console.WriteLine("Binary Heap with values 5, 2, 1, 21, 12, 2, 4 added");
+        binaryHeap.testInvariants();
+
+        System.Console.WriteLine("Binary Max Heap with values added 2, 16, 12, 7, 5, 12, 1 using add() function");
         binaryHeap.printList();
         System.Console.WriteLine();
 
         System.Console.WriteLine("Binary Heap popTop assert test");
         //loop pops until heap is empty and asserts each value popped is larger than previous popped value
+        //also asserts invariants are kept through each iteration
         for (int previousPop = binaryHeap.popTop(); binaryHeap.heapList.Count != 1;)
         {
             binaryHeap.printList();
-            int currentPop = binaryHeap.popTop();
-            Debug.Assert(previousPop <= currentPop);
-            previousPop = currentPop;
             System.Console.WriteLine();
+            int currentPop = binaryHeap.popTop();
+            Debug.Assert(previousPop >= currentPop);
+            binaryHeap.testInvariants();
+            previousPop = currentPop;
         }
 
-        System.Console.WriteLine("Heap empty");
+        List<int> myList = new List<int>(new int[] { 0, 5, 2, 1, 21, 12, 2, 4, 8});
+        binaryHeap.buildHeap(myList);
+
+        binaryHeap.testInvariants();
+
+        System.Console.WriteLine("Binary Max Heap with values 5, 2, 1, 21, 12, 2, 4 added using buildHeap() function");
+        binaryHeap.printList();
+        System.Console.WriteLine();
+
+        System.Console.WriteLine("Heap Sort");
+        binaryHeap.heapSort();
+        binaryHeap.printList();
+
+        for (int i = 0; i < binaryHeap.heapList.Count - 2; i++)
+        {
+            Debug.Assert(binaryHeap[i] <= binaryHeap[i + 1]);
+        }
     }
 }
